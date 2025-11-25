e750e75c500ae311a36b976f6990c82aaf5a8916
MWAMUZISCODE <caspermotive4@gmail.com>
2025-11-25 22:37:33 +0100

    modf #depo03

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
+function App() {
+
+
+  return (
+    <>
+    <div class='container'>
+      <h1> hello world </h1>
+    </div>
+    </>
+  )
+}
